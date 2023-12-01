09b899809974fb38b18a4f6efc830c6504ec9730
frederico.junior <frederico.junior@siriusgroup.com.br>
2023-12-01 18:28:50 -0300

    alterações musica

--- scripts/universe.js
+++ scripts/universe.js
@@ -1,28 +1,31 @@
-window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
-  window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+window.requestAnimationFrame =
+  window.requestAnimationFrame ||
+  window.mozRequestAnimationFrame ||
+  window.webkitRequestAnimationFrame ||
+  window.msRequestAnimationFrame;
 
-var starDensity = .216;
-var speedCoeff = .05;
+var starDensity = 0.216;
+var speedCoeff = 0.05;
 var width;
 var height;
 var starCount;
 var circleRadius;
 var circleCenter;
 var first = true;
-var giantColor = '180,184,240';
-var starColor = '226,225,142';
-var cometColor = '226,225,224';
-var canva = document.getElementById('universe');
+var giantColor = "180,184,240";
+var starColor = "226,225,142";
+var cometColor = "226,225,224";
+var canva = document.getElementById("universe");
 var stars = [];
 var universe;
 
 windowResizeHandler();
-window.addEventListener('resize', windowResizeHandler, false);
+window.addEventListener("resize", windowResizeHandler, false);
 
 createUniverse();
 
 function createUniverse() {
-  universe = canva.getContext('2d');
+  universe = canva.getContext("2d");
 
   for (var i = 0; i < starCount; i++) {
     stars[i] = new Star();
@@ -49,20 +52,24 @@ function draw() {
 }
 
 function Star() {
-
   this.reset = function () {
     this.giant = getProbability(3);
     this.comet = this.giant || first ? false : getProbability(10);
     this.x = getRandInterval(0, width - 10);
     this.y = getRandInterval(0, height);
     this.r = getRandInterval(1.1, 2.6);
-    this.dx = getRandInterval(speedCoeff, 6 * speedCoeff) + (this.comet + 1 - 1) * speedCoeff * getRandInterval(50, 120) + speedCoeff * 2;
-    this.dy = -getRandInterval(speedCoeff, 6 * speedCoeff) - (this.comet + 1 - 1) * speedCoeff * getRandInterval(50, 120);
+    this.dx =
+      getRandInterval(speedCoeff, 6 * speedCoeff) +
+      (this.comet + 1 - 1) * speedCoeff * getRandInterval(50, 120) +
+      speedCoeff * 2;
+    this.dy =
+      -getRandInterval(speedCoeff, 6 * speedCoeff) -
+      (this.comet + 1 - 1) * speedCoeff * getRandInterval(50, 120);
     this.fadingOut = null;
     this.fadingIn = true;
     this.opacity = 0;
-    this.opacityTresh = getRandInterval(.2, 1 - (this.comet + 1 - 1) * .4);
-    this.do = getRandInterval(0.0005, 0.002) + (this.comet + 1 - 1) * .001;
+    this.opacityTresh = getRandInterval(0.2, 1 - (this.comet + 1 - 1) * 0.4);
+    this.do = getRandInterval(0.0005, 0.002) + (this.comet + 1 - 1) * 0.001;
   };
 
   this.fadeIn = function () {
@@ -87,20 +94,30 @@ function Star() {
     universe.beginPath();
 
     if (this.giant) {
-      universe.fillStyle = 'rgba(' + giantColor + ',' + this.opacity + ')';
+      universe.fillStyle = "rgba(" + giantColor + "," + this.opacity + ")";
       universe.arc(this.x, this.y, 2, 0, 2 * Math.PI, false);
     } else if (this.comet) {
-      universe.fillStyle = 'rgba(' + cometColor + ',' + this.opacity + ')';
+      universe.fillStyle = "rgba(" + cometColor + "," + this.opacity + ")";
       universe.arc(this.x, this.y, 1.5, 0, 2 * Math.PI, false);
 
       //comet tail
       for (var i = 0; i < 30; i++) {
-        universe.fillStyle = 'rgba(' + cometColor + ',' + (this.opacity - (this.opacity / 20) * i) + ')';
-        universe.rect(this.x - this.dx / 4 * i, this.y - this.dy / 4 * i - 2, 2, 2);
+        universe.fillStyle =
+          "rgba(" +
+          cometColor +
+          "," +
+          (this.opacity - (this.opacity / 20) * i) +
+          ")";
+        universe.rect(
+          this.x - (this.dx / 4) * i,
+          this.y - (this.dy / 4) * i - 2,
+          2,
+          2
+        );
         universe.fill();
       }
     } else {
-      universe.fillStyle = 'rgba(' + starColor + ',' + this.opacity + ')';
+      universe.fillStyle = "rgba(" + starColor + "," + this.opacity + ")";
       universe.rect(this.x, this.y, this.r, this.r);
     }
 
@@ -114,7 +131,7 @@ function Star() {
     if (this.fadingOut === false) {
       this.reset();
     }
-    if (this.x > width - (width / 4) || this.y < 0) {
+    if (this.x > width - width / 4 || this.y < 0) {
       this.fadingOut = true;
     }
   };
@@ -122,16 +139,16 @@ function Star() {
   (function () {
     setTimeout(function () {
       first = false;
-    }, 50)
-  })()
+    }, 50);
+  })();
 }
 
 function getProbability(percents) {
-  return ((Math.floor(Math.random() * 1000) + 1) < percents * 10);
+  return Math.floor(Math.random() * 1000) + 1 < percents * 10;
 }
 
 function getRandInterval(min, max) {
-  return (Math.random() * (max - min) + min);
+  return Math.random() * (max - min) + min;
 }
 
 function windowResizeHandler() {
@@ -139,12 +156,12 @@ function windowResizeHandler() {
   height = window.innerHeight;
   starCount = width * starDensity;
   // console.log(starCount)
-  circleRadius = (width > height ? height / 2 : width / 2);
+  circleRadius = width > height ? height / 2 : width / 2;
   circleCenter = {
     x: width / 2,
-    y: height / 2
-  }
+    y: height / 2,
+  };
 
-  canva.setAttribute('width', width);
-  canva.setAttribute('height', height);
+  canva.setAttribute("width", width);
+  canva.setAttribute("height", height);
 }
